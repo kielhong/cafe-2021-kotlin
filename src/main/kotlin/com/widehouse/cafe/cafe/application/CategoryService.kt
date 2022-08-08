5f624fea61c562b4140afbf889b4afd2f53656d0
홍민형 <mh.hong@woowahan.com>
2022-08-08 18:09:36 +0900

    feat : listOrder 기준 sort 추가

--- src/main/kotlin/com/widehouse/cafe/cafe/application/CategoryService.kt
+++ src/main/kotlin/com/widehouse/cafe/cafe/application/CategoryService.kt
@@ -5,6 +5,7 @@ import com.widehouse.cafe.cafe.application.port.out.CategoryRepository
 import com.widehouse.cafe.cafe.domain.Category
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
+import java.util.Comparator.comparing
 
 @Service
 class CategoryService(
@@ -12,4 +13,5 @@ class CategoryService(
 ) : CategoryQueryUseCase {
     override fun listCategories(): Flux<Category> =
         categoryRepository.loadAllCategory()
+            .sort(comparing(Category::listOrder))
 }
