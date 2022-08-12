c6808a275841fae1ad8195585ab5c7027ea6bc2b
홍민형 <mh.hong@woowahan.com>
2022-08-12 15:07:32 +0900

    refactor : reqeust -> domain 변환 방식 변경

--- src/main/kotlin/com/widehouse/cafe/cafe/adapter/in/web/CafeRequest.kt
+++ src/main/kotlin/com/widehouse/cafe/cafe/adapter/in/web/CafeRequest.kt
@@ -1,13 +1,8 @@
 package com.widehouse.cafe.cafe.adapter.`in`.web
 
-import com.widehouse.cafe.cafe.domain.Cafe
-
 data class CafeRequest(
-    var id: String?,
+    val id: String?,
     val name: String,
     val description: String,
     val categoryId: Long
-) {
-    fun toDomain(): Cafe =
-        Cafe(id!!, name, description, categoryId)
-}
+)
