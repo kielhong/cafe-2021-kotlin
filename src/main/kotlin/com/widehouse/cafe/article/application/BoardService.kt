aec0b331b22a74dbf87a36ba321962b6b14d47fc
홍민형 <mh.hong@woowahan.com>
2022-08-18 20:24:59 +0900

    BoardController POST /boards

--- src/main/kotlin/com/widehouse/cafe/article/application/BoardService.kt
+++ src/main/kotlin/com/widehouse/cafe/article/application/BoardService.kt
@@ -1,5 +1,6 @@
 package com.widehouse.cafe.article.application
 
+import com.widehouse.cafe.article.adapter.`in`.web.dto.BoardRequest
 import com.widehouse.cafe.article.adapter.out.persistence.BoardRepository
 import com.widehouse.cafe.article.domain.Board
 import org.springframework.stereotype.Service
@@ -19,4 +20,9 @@ class BoardService(
     fun listBoard(cafeId: String): Flux<Board> =
         boardRepository.findByCafeId(cafeId)
             .sort(Comparator.comparingInt(Board::listOrder))
+
+    @Transactional
+    fun createBoard(request: BoardRequest): Mono<Board> {
+        TODO("Not yet implemented")
+    }
 }
--- src/test/kotlin/com/widehouse/cafe/article/adapter/in/web/BoardControllerTest.kt
+++ src/test/kotlin/com/widehouse/cafe/article/adapter/in/web/BoardControllerTest.kt
@@ -1,84 +1,119 @@
 package com.widehouse.cafe.article.adapter.`in`.web
 
+import com.ninjasquad.springmockk.MockkBean
 import com.widehouse.cafe.article.BoardFixtures
+import com.widehouse.cafe.article.adapter.`in`.web.dto.BoardRequest
 import com.widehouse.cafe.article.application.BoardService
 import com.widehouse.cafe.article.domain.Board
 import com.widehouse.cafe.cafe.CafeFixtures
-import com.widehouse.cafe.cafe.domain.Cafe
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.spring.SpringListener
+import io.mockk.every
+import io.mockk.verify
 import org.assertj.core.api.BDDAssertions.then
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.WebTestClient.ListBodySpec
+import org.springframework.web.reactive.function.BodyInserters
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @WebFluxTest(BoardController::class)
-internal class BoardControllerTest(@Autowired val webClient: WebTestClient) {
-    @MockBean
+internal class BoardControllerTest : DescribeSpec({
+    isolationMode = IsolationMode.InstancePerLeaf
+}) {
+    override fun listeners() = listOf(SpringListener)
+
+    @Autowired
+    private lateinit var webClient: WebTestClient
+
+    @MockkBean
     lateinit var boardService: BoardService
 
-    lateinit var cafe: Cafe
+    val cafe = CafeFixtures.create()
 
-    @BeforeEach
-    fun setUp() {
-        cafe = CafeFixtures.create()
-    }
-
-    @Nested
-    inner class GetBoard {
-        @Test
-        fun given_boardId_when_get_then_returnBoard() {
-            // given
-            val board = BoardFixtures.create("test", cafe.id)
-            given(boardService.getBoard(board.id)).willReturn(Mono.just(board))
-            // when
-            webClient.get()
-                .uri("/boards/{boardId}", board.id)
-                .exchange()
-                .expectStatus().isOk
-                .expectBody()
-                .jsonPath("$.id").isEqualTo(board.id)
-                .jsonPath("$.cafeId").isEqualTo(cafe.id)
-        }
-
-        @Test
-        fun given_empty_when_get_then_404NotFound() {
-            // given
-            val boardId = "1234"
-            given(boardService.getBoard(boardId)).willReturn(Mono.empty())
-            // when
-            webClient.get()
-                .uri {
-                    it.path("/boards/{boardId}")
-                        .build(boardId)
+    init {
+        describe("GetBoard") {
+            val boardId = "test"
+            context("get board by boardId") {
+                val board = BoardFixtures.create(boardId, cafe.id)
+                every { boardService.getBoard(board.id) } returns Mono.just(board)
+                it("should return board") {
+                    // when
+                    webClient
+                        .get()
+                        .uri("/boards/{boardId}", board.id)
+                        .exchange()
+                        .expectStatus().isOk
+                        .expectBody()
+                        .jsonPath("$.id").isEqualTo(board.id)
+                        .jsonPath("$.cafeId").isEqualTo(cafe.id)
                 }
-                .exchange()
-                .expectStatus().isNotFound
-        }
-    }
-
-    @Test
-    fun given_cafeId_when_listByCafe_then_listBoards() {
-        // given
-        val board1 = BoardFixtures.create("1", cafe.id)
-        val board2 = BoardFixtures.create("2", cafe.id)
-        given(boardService.listBoard(cafe.id)).willReturn(Flux.just(board1, board2))
-        // when
-        webClient.get()
-            .uri {
-                it.path("/boards")
-                    .queryParam("cafeId", cafe.id)
-                    .build()
             }
-            .exchange()
-            .expectStatus().isOk
-            .expectBodyList(Board::class.java)
-            .consumeWith<ListBodySpec<Board>> { then(it.responseBody).containsExactly(board1, board2) }
+
+            context("do not get board by boardId") {
+                it("should return 404 Not Found") {
+                    // given
+                    every { boardService.getBoard(boardId) } returns Mono.empty()
+                    // when
+                    webClient.get()
+                        .uri {
+                            it.path("/boards/{boardId}")
+                                .build(boardId)
+                        }
+                        .exchange()
+                        .expectStatus().isNotFound
+                }
+            }
+        }
+
+        describe("listBoard by Cafe") {
+            it("should return board lists") {
+                // given
+                val board1 = BoardFixtures.create("1", cafe.id)
+                val board2 = BoardFixtures.create("2", cafe.id)
+                every { boardService.listBoard(cafe.id) } returns Flux.just(board1, board2)
+                // when
+                webClient
+                    .get()
+                    .uri {
+                        it.path("/boards")
+                            .queryParam("cafeId", cafe.id)
+                            .build()
+                    }
+                    .exchange()
+                    .expectStatus().isOk
+                    .expectBodyList(Board::class.java)
+                    .consumeWith<ListBodySpec<Board>> { then(it.responseBody).containsExactly(board1, board2) }
+            }
+        }
+
+        describe("post Board") {
+            context("given request") {
+                val request = BoardRequest(cafe.id, "board", 1)
+                every { boardService.createBoard(any()) } returns Mono.just(BoardFixtures.create("test", request.cafeId))
+
+                val response = webClient
+                    .post()
+                    .uri("/boards")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(BodyInserters.fromValue(request))
+                    .exchange()
+                it("200 OK") {
+                    response
+                        .expectStatus().isOk
+                    verify { boardService.createBoard(request) }
+                }
+                it("return created board") {
+                    response
+                        .expectBody()
+                        .jsonPath("$.id").isEqualTo("test")
+                    verify { boardService.createBoard(request) }
+                }
+            }
+        }
     }
 }
