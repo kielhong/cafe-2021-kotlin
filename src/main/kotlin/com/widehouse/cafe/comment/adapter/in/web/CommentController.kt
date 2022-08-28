9aba6a146197e32f23e886125ca87fa5f39df437
홍민형 <mh.hong@woowahan.com>
2022-08-28 22:45:30 +0900

    fix : ktlint

--- src/main/kotlin/com/widehouse/cafe/comment/adapter/in/web/CommentController.kt
+++ src/main/kotlin/com/widehouse/cafe/comment/adapter/in/web/CommentController.kt
@@ -3,7 +3,6 @@ package com.widehouse.cafe.comment.adapter.`in`.web
 import com.widehouse.cafe.comment.application.CommentQueryUseCase
 import com.widehouse.cafe.comment.domain.Comment
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
